376414c1caf83a9f805cf2ffc2e872e9193a05db
Gnosis <emmagab38@gmail.com>
2023-11-17 22:06:05 +0100

    commit changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         Blade::directive('money', function ($amount) {
             return "<?php echo number_format($amount, 2); ?>";
         });
--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -136,6 +136,24 @@
                             </li>
                         </ul>
                     </li>
+                    <li class="nav-main-item">
+                        <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
+                            <i class="nav-main-link-icon fa fa-house-user"></i>
+                            <span class="nav-main-link-name">Real Estate</span>
+                        </a>
+                        <ul class="nav-main-submenu">
+                            <li class="nav-main-item">
+                                <a class="nav-main-link" href="{{ route('admin.stock.index') }}">
+                                    <span class="nav-main-link-name">Invested Properties</span>
+                                </a>
+                            </li>
+                            <li class="nav-main-item">
+                                <a class="nav-main-link" href="{{ route('admin.stock.create') }}">
+                                    <span class="nav-main-link-name">Add Property</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
 
                     <li class="nav-main-heading">Extend</li>
 
