90f21b54392918500c68e642e69883471dcbb65a
Gnosis <emmagab38@gmail.com>
2023-11-29 15:34:53 +0100

    commit changes

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Property extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function images()
+    {
+        return $this->hasMany(Image::class, 'properties_id');
+    }
+
+
+    public function invest()
+    {
+        return $this->hasMany(Inves::class);
+    }
+
 }
--- resources/views/dashboard/property/list.blade.php
+++ resources/views/dashboard/property/list.blade.php
@@ -7,37 +7,38 @@
                 <div class="nk-block-head nk-block-head-sm">
                     <div class="nk-block-between g-3">
                         <div class="nk-block-head-content">
-                            <h3 class="nk-block-title page-title">Image Gallery</h3>
-                            <div class="nk-block-des text-soft">
-                                <p>You have total <span class="text-base">1,257</span> Media.</p>
-                            </div>
-                        </div>
-                        <div class="nk-block-head-content">
-                            <a href="#" class="btn btn-outline-light bg-white d-none d-sm-inline-flex"><em class="icon ni ni-download-cloud"></em><span>Download All</span></a>
-                            <a href="#" class="btn btn-icon btn-outline-light bg-white d-inline-flex d-sm-none"><em class="icon ni ni-download-cloud"></em></a>
+                            <h3 class="nk-block-title page-title ">All Listed Properties</h3>
+                            <p>Explore Our Diverse Collection of Premier Real Estate Listings</p>
                         </div>
                     </div>
                 </div><!-- .nk-block-head -->
-                <div class="nk-block">
+                <div class="nk-block mt-5">
                     <div class="row g-gs">
                         @foreach($properties as $item)
                             <div class="col-sm-6 col-lg-4">
                             <div class="gallery card card-bordered">
                                 <a class="gallery-image popup-image" href="./images/stock/a.jpg">
-                                    <img class="w-100 rounded-top" src="{{ asset('dash/assets/images/stock/a.jpg') }}" alt="">
+                                    @foreach ($item->images as $image)
+                                        @if($loop->first)
+                                            <div class="new-arrivals-img-contnent">
+                                                <img class="w-100 rounded-top" src="{{ asset($image->image_path) }}" alt="{{ $item->name }}">
+                                            </div>
+                                        @endif
+                                    @endforeach
+
                                 </a>
                                 <div class="gallery-body card-inner align-center justify-between flex-wrap g-2">
                                     <div class="user-card">
-                                        <div class="user-avatar">
-                                            <img src="./images/avatar/a-sm.jpg" alt="">
-                                        </div>
                                         <div class="user-info">
-                                            <span class="lead-text">Dustin Mock</span>
-                                            <span class="sub-text">mock@softnio.com</span>
+                                            <h3 class="">{{ $item->name }}</h3>
+                                            <h5 class="text-lead mt-3">@money($item->price) <span class="text-success">{{ auth()->user()->currency }}</span></h5>
+                                            <br><br>
+                                            <span class="text-lead">Min: </span><span class="text-white mt-5">@money($item->min_price) <span class="text-success">{{ auth()->user()->currency }}</span></span><br>
+                                            <span class="text-lead"><i class="icon ni ni-location"></i> </span><span class="text-white mt-5">{{ $item->building_location }} </span>
                                         </div>
                                     </div>
                                     <div>
-                                        <button class="btn btn-p-0 btn-nofocus"><em class="icon ni ni-heart"></em><span>34</span></button>
+                                        <button class="btn btn-p-0 btn-nofocus"><em class="icon ni ni-heart-fill"></em><span>{{ $item->reviews }}</span></button>
                                     </div>
                                 </div>
                             </div>
